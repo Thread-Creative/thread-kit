1df98abb4603a3b3c719327cec5731f928d6d9de
kegansovay <kegan.sovay@gmail.com>
2025-10-17 10:21:09 -0700

    updates to latest plugin

--- package.config.ts
+++ package.config.ts
@@ -1,7 +1,15 @@
 import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
-  bundles: [{source: './src/cli.ts', require: './dist/cli.js'}],
-  runtime: 'node',
+  dist: 'dist',
   tsconfig: 'tsconfig.dist.json',
+
+  // Remove this block to enable strict export validation
+  extract: {
+    rules: {
+      'ae-incompatible-release-tags': 'off',
+      'ae-internal-missing-underscore': 'off',
+      'ae-missing-release-tag': 'off',
+    },
+  },
 })
